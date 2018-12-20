789680e55797ed0d69f70c87d181b00341336d09
takecian <takecian@gmail.com>
2018-12-20 17:39:47 +0900

    Create project

---
+++ ComposableStyling/ComposableStyling.h
@@ -0,0 +1,19 @@
+//
+//  ComposableStyling.h
+//  ComposableStyling
+//
+//  Created by Fujiki Takeshi on 2018/12/20.
+//  Copyright © 2018 takecian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ComposableStyling.
+FOUNDATION_EXPORT double ComposableStylingVersionNumber;
+
+//! Project version string for ComposableStyling.
+FOUNDATION_EXPORT const unsigned char ComposableStylingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ComposableStyling/PublicHeader.h>
+
+
